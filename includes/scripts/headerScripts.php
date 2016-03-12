6568108744d9dc33f4f83314bec670cfbc59973f
lahiru <lahirupathiranalr@gmail.com>
2016-03-12 21:56:21 +0530

    scripts

---
+++ includes/scripts/headerScripts.php
@@ -0,0 +1,23 @@
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+
+<!-- Favicon Zone -->
+<link rel="icon" href="img/favicon.png" />
+<!-- Style & bootstrap import -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/shop-homepage.css" rel="stylesheet">
+<link href="css/agency.css" rel="stylesheet">
+
+<!-- dhanushka -->
+<link href="css/dc.css" rel="stylesheet">
+
+<!-- End of inmport -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
